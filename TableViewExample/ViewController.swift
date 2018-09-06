80312adda37f584297df2e20f234ded1f6a8ccc6
yoga <yogamadness@gmail.com>
2018-09-06 13:18:33 +0700

    Initial Commit

---
+++ TableViewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewExample
+//
+//  Created by Yoga Pratama on 06/09/18.
+//  Copyright © 2018 YPA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
