7af5d3a063df753d2749f83369329e5df79db3d6
yoga <yogamadness@gmail.com>
2018-09-06 16:39:59 +0700

    Code For UITableview and UITableViewCell

---
+++ TableViewExample/MainTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  MainTableViewCell.swift
+//  TableViewExample
+//
+//  Created by Yoga Pratama on 06/09/18.
+//  Copyright © 2018 YPA. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+    
+    @IBOutlet var playerName : UILabel!
+    @IBOutlet var playerAge : UILabel!
+    @IBOutlet var playerPos : UILabel!
+    @IBOutlet var playerImage : UIImageView!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- TableViewExample/MainTableViewController.swift
+++ TableViewExample/MainTableViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class MainTableViewController: UITableViewController {
-
+    
+ /*  var playerName =  ["Jordan Henderson" , "Mohammed Salah", "Sadio Mane" , "Roberto Firmino",
+                       "Allison Becker" , "Naby Keita" , "Xherdan Shaqiri","Virgil Van Dijk",
+                       "Trent Alexander Arnold" , "Joe Gomez" , "Dejan Lovren" , "Simon Mignolet",
+                       "Andrew Robertson" , "Fabinho" , "James Milner" , "Daniel Sturridge","Alberto Moreno", "Joel Matip"
+                      ] */
+    
+    var playerName =  ["Jordan Henderson" , "Mohammed Salah", "Sadio Mane" , "Roberto Firmino",
+                      ]
+    var playerImage = ["henderson","salah","mane","firmino"]
+    var playerAge = ["28" , "26" , "26" , "27"]
+    var playerPos = ["Midfielder" , "Winger", "Winger" , "Striker"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -29,23 +41,31 @@ class MainTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return playerName.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        let cellIdentifier = "cell"
+        // downcast to cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! MainTableViewCell
 
         // Configure the cell...
-
+       // cell.textLabel?.text = playerName[indexPath.row]
+       // cell.imageView?.image = UIImage(named: playerImage[indexPath.row])
+          cell.playerName.text = playerName[indexPath.row]
+          cell.playerImage.image = UIImage(named: playerImage[indexPath.row])
+          cell.playerAge.text = playerAge[indexPath.row]
+          cell.playerPos.text = playerPos[indexPath.row]
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
