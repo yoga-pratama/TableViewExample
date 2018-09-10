87b3eeb0c1e5ceceb78ef8fdadfe92872faf0bff
yoga <yogamadness@gmail.com>
2018-09-10 10:39:35 +0700

    enable edit cell

--- TableViewExample/MainTableViewController.swift
+++ TableViewExample/MainTableViewController.swift
@@ -89,7 +89,7 @@ class MainTableViewController: UITableViewController {
     }
     */
 
-    /*
+
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -99,7 +99,7 @@ class MainTableViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+
 
     /*
     // Override to support rearranging the table view.
